f62564680a4370c1e05c393a238ca1a1165b75a5
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2024-03-21 14:50:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/admobnativead/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.admobnativead;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
+import com.google.android.ads.nativetemplates.TemplateView;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.nativead.NativeAd;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        MobileAds.initialize(this);
+        AdLoader adLoader = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
+                .forNativeAd(new NativeAd.OnNativeAdLoadedListener() {
+                    @Override
+                    public void onNativeAdLoaded(@NonNull NativeAd nativeAd) {
+                        NativeTemplateStyle style = new NativeTemplateStyle.Builder().build();
+                        TemplateView templateView = findViewById(R.id.nativeAdTemplate);
+                        templateView.setStyles(style);
+                        templateView.setNativeAd(nativeAd);
+                    }
+                }).build();
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+}
